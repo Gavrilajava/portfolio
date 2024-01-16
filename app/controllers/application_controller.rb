02d1eb5d78e3e2863e728df2741a3c95906ad8ce
George Gavrilchik <ggavrilchik@extendmed.com>
2024-01-16 16:47:21 -0600

    install-tailwind

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_navbar_links
+
+  def set_navbar_links
+    @navbar_links = {
+      home: root_path,
+      groceries: groceries_path,
+      help: help_path,
+    }
+  end
 end
